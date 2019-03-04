ad1a7cba27a3963ffe33d8b3596588bf6bc13da2
Kathy Chiem <kchie002@gmail.com>
2019-03-03 22:18:21 -0800

    put words and hints into paired objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,49 +10,75 @@ const playMusic = () => {
 startButton.addEventListener('click', playMusic)
 
 //declare variables
-const alphabet = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphabet = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let guess ;
 let storedGuesses = []; 
-let lives = document.querySelector("#lives-count");
-const clueList = [
-    `Who said "You know nothing, Jon Snow"?`,//Ygritte
-    `Who said "A lion does not concern himself with the opinion of sheep"?`,//Tywin Lannister
-    `Who said "The things I do for love"?`,//Jamie Lannister
-    `A "traitor"`,//Ned Stark
-    `A girl is no one`, //Arya Stark
-    `Who said "Everyone is mine to torment"?`, //Joffrey Baratheon
-    `The Mountain`, //Gregore Clegane
-    `Who said "Power is power"?`, //Cersei Lannister
-    `Mother of Dragons` //Daenerys Targaryen
-];
-let wordList = [
-    `Ygritte`,
-    `Tywin`,
-    `Jamie`,
-    `Ned`,
-    `Arya`,
-    `Joffrey`,
-    `Gregore`,
-    `Cersei`,
-    `Daenerys`
-];
+let lives ;
+// let clueList = [
+//     `Who said "You know nothing, Jon Snow"?`,//Ygritte
+//     `Who said "A lion does not concern himself with the opinion of sheep"?`,//Tywin Lannister
+//     `Who said "The things I do for love"?`,//Jamie Lannister
+//     `A "traitor"`,//Ned Stark
+//     `A girl is no one`, //Arya Stark
+//     `Who said "Everyone is mine to torment"?`, //Joffrey Baratheon
+//     `The Mountain`, //Gregore Clegane
+//     `Who said "Power is power"?`, //Cersei Lannister
+//     `Mother of Dragons` //Daenerys Targaryen
+// ];
+// let wordList = [
+//     `Ygritte`,
+//     `Tywin`,
+//     `Jamie`,
+//     `Ned`,
+//     `Arya`,
+//     `Joffrey`,
+//     `Gregore`,
+//     `Cersei`,
+//     `Daenerys`
+// ];
+let wordList = [{
+    hint: `"You know nothing, Jon Snow"`,
+    answer: "Ygritte"
+},
+                {
+    hint: `A girl is no one`,
+    answer: 'Arya'
+},
+                {
+    hint: 'A bastard son',
+    answer: 'Gendry'
+},
+                {
+    hint: '"_____ is power" - Cersei Lannister',
+    answer: 'Power'
+},
+                {
+    hint: 'West of the Narrow Sea',
+    answer: 'Westeros'
+},
+                {
+    hint: 'Largest dragon',
+    answer: 'Drogon'
+}
+]
 
-//randomized clue variable
-let randomClue = clueList[Math.floor(Math.random() * clueList.length)];
+// const randomClue = clueList[Math.floor(Math.random() * clueList.length)];
 const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-//show clue 
+//functions
 const showClue = () => {
-    document.querySelector("#clue").textContent = "Clue: " + (randomClue);
+    document.querySelector("#clue").textContent = "Clue: " + (randomWord.hint);
+    console.log(randomWord.hint)
+    console.log(randomWord.answer)
 }
 
 const showUnderscores = () => {
-    for (let i = 0; i < randomWord.length; i++) {
+    for (let i = 0; i < randomWord.answer.length; i++) {
     let thing = document.createElement('span');
     thing.textContent = '_ ';
     document.querySelector('#word-box').append(thing);
 }}
 
-//clue onClick event
+//function calls
 startButton.addEventListener('click', showClue);
 startButton.addEventListener('click', showUnderscores);
