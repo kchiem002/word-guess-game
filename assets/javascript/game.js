1b4df5d9355ea35c336ef383ecf58a9a0ca11d68
Kathy Chiem <kchie002@gmail.com>
2019-03-05 18:23:35 -0800

    added buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,28 +15,22 @@ let guess ;
 let storedGuesses = "A, B, C";
 let storedWins = 0;
 let lives = 10;
-// let clueList = [
-//     `Who said "You know nothing, Jon Snow"?`,//Ygritte
-//     `Who said "A lion does not concern himself with the opinion of sheep"?`,//Tywin Lannister
-//     `Who said "The things I do for love"?`,//Jamie Lannister
-//     `A "traitor"`,//Ned Stark
-//     `A girl is no one`, //Arya Stark
-//     `Who said "Everyone is mine to torment"?`, //Joffrey Baratheon
-//     `The Mountain`, //Gregore Clegane
-//     `Who said "Power is power"?`, //Cersei Lannister
-//     `Mother of Dragons` //Daenerys Targaryen
-// ];
-// let wordList = [
-//     `Ygritte`,
-//     `Tywin`,
-//     `Jamie`,
-//     `Ned`,
-//     `Arya`,
-//     `Joffrey`,
-//     `Gregore`,
-//     `Cersei`,
-//     `Daenerys`
-// ];
+let buttonA = document.querySelector("#button-a");
+
+function checkUserGuess(e) {
+    console.log(e.target.innerHTML)
+}
+
+function initializeButtons() {
+    for (let i = 0; i < alphabet.length; i++) {
+
+        let guessButton = document.createElement('button')
+        guessButton.innerHTML = alphabet[i]
+        guessButton.onclick = checkUserGuess
+        document.querySelector('.alphabet-box').append(guessButton)
+    }
+}
+initializeButtons()
 let wordList = [{
     hint: `"You know nothing, Jon Snow"`,
     answer: "Ygritte"
@@ -61,28 +55,47 @@ let wordList = [{
     hint: 'Largest dragon',
     answer: 'Drogon'
 }
-]
-
+];
 // const randomClue = clueList[Math.floor(Math.random() * clueList.length)];
 const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+let remainingLetters = randomWord.answer.length;
 
 //functions
 const showClue = () => {
     document.querySelector("#clue").textContent = "Clue: " + (randomWord.hint);
     console.log(randomWord.hint)
     console.log(randomWord.answer)
-}
+};
 
-const showUnderscores = () => {
+const answerArray = () => {
     for (let i = 0; i < randomWord.answer.length; i++) {
     let thing = document.createElement('span');
     thing.textContent = '_ ';
     document.querySelector('#word-box').append(thing);
-}}
+}};
+
+const guessA = () => {
+    console.log("a")
+    return("a")
+};
+
+const gameLoop = () => {
+    if (guessA === randomWord.answer.match) {
+        console.log("true");
+        let thing = document.createElement('span');
+        this.textContent = "a";
+        document.querySelector("#word-box").append(thing);
+    }
+    else {
+        console.log("false")
+    }
+};
 
 //function calls
 startButton.addEventListener('click', showClue);
-startButton.addEventListener('click', showUnderscores);
+startButton.addEventListener('click', answerArray);
 document.querySelector('#lives-count-js').append(lives);
 document.querySelector('#lives-lost-count-js').append(storedGuesses);
 document.querySelector('#wins-count-js').append(storedWins);
+buttonA.addEventListener('click', guessA);
+buttonA.addEventListener('click', gameLoop);
